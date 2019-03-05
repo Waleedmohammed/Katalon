f00f0eba8cfba3f0305442676ec3e778e2fcfd9b
Etisalat <waleed.m.science@gmail.com>
2019-03-05 20:40:36 +0400

    Add

---
+++ Test Suites/TS2.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6da948bf-0814-490b-857e-db4673a53f2e</testSuiteGuid>
+   <testCaseLink>
+      <guid>32a02ff6-3c63-43a0-8c7e-798b834d854b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
